e51588ea601f4431a7017fc641c83dae856ee467
Jayaseelan Anbazhagan <jayaseelan.anbazhagan@ace-alternatives.com>
2022-12-25 13:44:24 +0530

    add new note component added with grid layout - css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import NoteList from "./components/NotesList";
+import { nanoid } from "nanoid";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "26/12/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+      date: "25/12/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "24/12/2022",
+    },
+    {
+      id: nanoid(),
+      text: "This is my new note",
+      date: "23/12/2022",
+    },
+  ]);
   return (
     <div className="container">
-      <NoteList />
+      <NoteList notes={notes} />
     </div>
   );
 };
